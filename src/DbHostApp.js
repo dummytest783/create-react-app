9d5dc743595cd6c2d4b90fc7573f8f2bd98c8fa9
ababbasi <ababbasi@ebay.com>
2023-02-26 02:03:55 -0800

    Display data

--- src/DbHostApp.js
+++ src/DbHostApp.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header, Table } from 'semantic-ui-react'
+import { Header, Table, Input, Button } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import './DbHostApp.css'
 import dbData from './hostdata.json';
@@ -9,57 +9,117 @@ import axios from 'axios';
 class DbHostApp extends React.Component {
     constructor(props) {
         super(props);
-        console.log(dbData);
-        this.state = {
+        this.valuationList = [
+          'PERatio',
+          'PEGRatio',
+          'EPS',
+          'DividendYield'
+        ];
+        this.profitList = [
+            'ProfitMargin',
+            'ReturnOnAssetsTTM',
+            'ReturnOnEquityTTM'
+        ];
 
+        this.state = {
+          inputTicker :'',
+          result: []
         };
     }
-    componentDidMount() {
-      const symbol = 'MSFT';
+
+    getTickerData (inputArray) {
       const apikey = 'OKK7K4SBZYR5UV7N';
       
-      const apiUrl = `https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=${symbol}&apikey=${apikey}`;
-      axios.get(apiUrl)
-        .then(response => {
-          console.log('micro', response.data);
-        })
-        .catch(error => {
-          console.log(error);
+      // const apiUrl = `https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=${symbol}&apikey=${apikey}`;
+      // axios.get(apiUrl)
+      //   .then(response => {
+      //     console.log('micro', response.data);
+      //   })
+      //   .catch(error => {
+      //     console.log(error);
+      // });
+      const requestPromises = [];
+      for (const ticker of inputArray) {
+        const apiUrl = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey=${apikey}`;
+        requestPromises.push(axios.get(apiUrl))
+      }
+
+      Promise.all(requestPromises).then((values) => {
+        console.log(values);
+        this.setState({result: values.map(value => value.data)})
       });
-      
+
     }
+
+    handleTickerChange (e) {
+      this.setState({inputTicker: e.target.value})
+    }
+
+    searchClick() {
+      const tickerStr = this.state.inputTicker.replace(/\s+/g, "");
+      const inputArray = tickerStr.split(',');
+      console.log(inputArray)
+      this.getTickerData(inputArray)
+
+    }
+
     render() {
       return  (
         <div> 
           <div className="dbdata">
-            <Header as='h1'> DB Status Dashboard</Header>
-            <div className="dbtable">
+            <Header as='h1'> Stock Dashboard</Header>
+            <Input placeholder='Enter coma seperated ticker' className='tickerInput' value={this.state.inputTicker} onChange={e => this.handleTickerChange(e)} />
+            <Button primary onClick={e => this.searchClick()}>Search</Button>
+            <div className="dbtable" >
               <Table>
                 <Table.Header>
                   <Table.Row>
-                    <Table.HeaderCell>Host Name</Table.HeaderCell>
-                    <Table.HeaderCell> Status</Table.HeaderCell>
-                    <Table.HeaderCell> Primary </Table.HeaderCell>
+                    <Table.HeaderCell>Indicator</Table.HeaderCell>
+                    {
+                      this.state.result && this.state.result.map((item, index) => {
+                        return (<Table.HeaderCell key={index}> {item.Symbol}</Table.HeaderCell>)
+                      })
+                    }
                   </Table.Row>
                 </Table.Header>
             
                 <Table.Body>
+                  <Table.Row fullWidth className='sectionHeader'> Valuation </Table.Row>
                     {
-                        dbData.hosts && dbData.hosts.map((item) => {
-                          return (<Table.Row key={item.hostname}>
-                              <Table.Cell> {item.hostname} </Table.Cell>
-                              <Table.Cell className = {item.status === 0 ? 'upcolor' : 'downcolor' } > {item.status === 0 ? 'Up' : 'Down' } </Table.Cell>
-                              <Table.Cell> {item.primary} </Table.Cell>
+                        this.valuationList && this.valuationList.map((item, index) => {
+                          return (<Table.Row key={index}>
+                              <Table.Cell> {item} </Table.Cell>
+                              {
+                                this.state.result && this.state.result.map((tickerDataObj, index) => {
+                                  return (<Table.Cell className = {item.status === 0 ? 'upcolor' : 'downcolor' } > {tickerDataObj[item]} </Table.Cell>)
+                                })
+                              }
                           </Table.Row>)
+                          
                         })
+                        
+                    }
+                     <Table.Row fullWidth className='sectionHeader'> Profitability </Table.Row>
+
+                     {
+                        this.profitList && this.profitList.map((item, index) => {
+                          return (<Table.Row key={index}>
+                              <Table.Cell> {item} </Table.Cell>
+                              {
+                                this.state.result && this.state.result.map((tickerDataObj, index) => {
+                                  return (<Table.Cell className = {item.status === 0 ? 'upcolor' : 'downcolor' } > {tickerDataObj[item]} </Table.Cell>)
+                                })
+                              }
+                          </Table.Row>)
+                          
+                        })
+                        
                     }
 
                 </Table.Body>
               </Table>
             </div>
-            <div className="updatetime"> Last updated timestamp : {dbData.lastUpdated} </div>
             </div>
-            <footer class="footer"> </footer>
           </div>
       )
     }
