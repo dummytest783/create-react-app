8438472394c2c529314dc51df4eded36e0069426
ababbasi <ababbasi@ebay.com>
2023-02-26 03:36:56 -0800

    Format and css

--- src/DbHostApp.js
+++ src/DbHostApp.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Header, Table, Input, Button } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import './DbHostApp.css'
-import dbData from './hostdata.json';
 import axios from 'axios';
 
 
@@ -10,15 +9,15 @@ class DbHostApp extends React.Component {
     constructor(props) {
         super(props);
         this.valuationList = [
-          'PERatio',
-          'PEGRatio',
-          'EPS',
-          'DividendYield'
+          {key: 'PERatio', better: 'lower'},
+          {key: 'PEGRatio', better: 'lower', desc: 'less than 1 is undervalued company'},
+          {key: 'EPS', better: 'higher'},
+          {key: 'DividendYield', better: 'higher'}
         ];
         this.profitList = [
-            'ProfitMargin',
-            'ReturnOnAssetsTTM',
-            'ReturnOnEquityTTM'
+            {key: 'ProfitMargin', better: 'higher'},
+            {key: 'ReturnOnAssetsTTM', better: 'higher'},
+            {key: 'ReturnOnEquityTTM', better: 'higher'}
         ];
 
         this.state = {
@@ -27,17 +26,20 @@ class DbHostApp extends React.Component {
         };
     }
 
+    isValueGood(item, value) {
+       const allNumbers = this.state.result.map(stock => parseFloat(stock[item.key]))
+       if(item.better === 'lower') {
+          return Math.min(...allNumbers) === parseFloat(value)
+       } else if (item.better === 'higher') {
+          return Math.max(...allNumbers) === parseFloat(value)
+       } else if(item.better === 'lessThan1') {
+          return parseFloat(value) < 1
+       }
+       return false;
+    }
+
     getTickerData (inputArray) {
       const apikey = 'OKK7K4SBZYR5UV7N';
-      
-      // const apiUrl = `https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=${symbol}&apikey=${apikey}`;
-      // axios.get(apiUrl)
-      //   .then(response => {
-      //     console.log('micro', response.data);
-      //   })
-      //   .catch(error => {
-      //     console.log(error);
-      // });
       const requestPromises = [];
       for (const ticker of inputArray) {
         const apiUrl = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey=${apikey}`;
@@ -59,6 +61,7 @@ class DbHostApp extends React.Component {
       const tickerStr = this.state.inputTicker.replace(/\s+/g, "");
       const inputArray = tickerStr.split(',');
       console.log(inputArray)
+      this.setState({'inputTicker': ''})
       this.getTickerData(inputArray)
 
     }
@@ -67,14 +70,14 @@ class DbHostApp extends React.Component {
       return  (
         <div> 
           <div className="dbdata">
-            <Header as='h1'> Stock Dashboard</Header>
+            <Header as='h1'> Which Stock to Buy? </Header>
             <Input placeholder='Enter coma seperated ticker' className='tickerInput' value={this.state.inputTicker} onChange={e => this.handleTickerChange(e)} />
-            <Button primary onClick={e => this.searchClick()}>Search</Button>
-            <div className="dbtable" >
-              <Table>
+            <Button className="searchbtn" primary onClick={e => this.searchClick()}>Search</Button>
+            <div>
+              <Table unstackable className="table">
                 <Table.Header>
                   <Table.Row>
-                    <Table.HeaderCell>Indicator</Table.HeaderCell>
+                    <Table.HeaderCell>Technical Indicator</Table.HeaderCell>
                     {
                       this.state.result && this.state.result.map((item, index) => {
                         return (<Table.HeaderCell key={index}> {item.Symbol}</Table.HeaderCell>)
@@ -88,10 +91,10 @@ class DbHostApp extends React.Component {
                     {
                         this.valuationList && this.valuationList.map((item, index) => {
                           return (<Table.Row key={index}>
-                              <Table.Cell> {item} </Table.Cell>
+                              <Table.Cell> {item.key} </Table.Cell>
                               {
                                 this.state.result && this.state.result.map((tickerDataObj, index) => {
-                                  return (<Table.Cell className = {item.status === 0 ? 'upcolor' : 'downcolor' } > {tickerDataObj[item]} </Table.Cell>)
+                                  return (<Table.Cell className = {this.isValueGood(item, tickerDataObj[item.key]) && 'upcolor' } > {tickerDataObj[item.key]} </Table.Cell>)
                                 })
                               }
                           </Table.Row>)
@@ -104,10 +107,10 @@ class DbHostApp extends React.Component {
                      {
                         this.profitList && this.profitList.map((item, index) => {
                           return (<Table.Row key={index}>
-                              <Table.Cell> {item} </Table.Cell>
+                              <Table.Cell> {item.key} </Table.Cell>
                               {
                                 this.state.result && this.state.result.map((tickerDataObj, index) => {
-                                  return (<Table.Cell className = {item.status === 0 ? 'upcolor' : 'downcolor' } > {tickerDataObj[item]} </Table.Cell>)
+                                  return (<Table.Cell className = {this.isValueGood(item, tickerDataObj[item.key]) && 'upcolor' }  > {tickerDataObj[item.key]} </Table.Cell>)
                                 })
                               }
                           </Table.Row>)
